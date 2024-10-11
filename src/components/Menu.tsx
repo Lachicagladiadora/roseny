bc743a5c31dedfc826accc93bbf77b2c85a6ee43
Lachicagladiadora <dev.roseny@gmail.com>
2024-10-11 10:59:15 -0500

    chore: remove unnecessary comments

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -10,16 +10,32 @@ export const Menu = () => {
     >
       <h3 className="py-6 text-lg font-bold text-center">Menu</h3>
       <ul className="flex flex-col justify-center">
-        <li className="py-4 px-2 text-center border-t-2 border-b-2 last:border-b-0 border-black dark:border-zinc-50">
+        <li
+          className="py-4 px-2 text-center border-t-2 border-b-2 last:border-b-0 border-black 
+        hover:bg-black hover:text-white
+        dark:border-zinc-50 dark:hover:bg-white dark:hover:text-black dark:hover:border-black"
+        >
           <a href="/">Home</a>
         </li>
-        <li className="py-4 px-2 text-center border-b-2 last:border-b-0 border-black dark:border-zinc-50">
+        <li
+          className="py-4 px-2 text-center border-b-2 last:border-b-0 border-black 
+        hover:bg-black hover:text-white
+        dark:border-zinc-50 dark:hover:bg-white dark:hover:text-black dark:hover:border-black"
+        >
           <a href="/about">About</a>
         </li>
-        <li className="py-4 px-2 text-center border-b-2 last:border-b-0 border-black dark:border-zinc-50">
+        <li
+          className="py-4 px-2 text-center border-b-2 last:border-b-0 border-black 
+        hover:bg-black hover:text-white
+        dark:border-zinc-50 dark:hover:bg-white dark:hover:text-black dark:hover:border-black"
+        >
           <a href="/blog">Blog</a>
         </li>
-        <li className="py-4 px-2 text-center border-b-2 last:border-b-0 border-black dark:border-zinc-50">
+        <li
+          className="py-4 px-2 text-center border-b-2 last:border-b-0 border-black 
+        hover:bg-black hover:text-white
+        dark:border-zinc-50 dark:hover:bg-white dark:hover:text-black dark:hover:border-black"
+        >
           <a href="/contact">Contact</a>
         </li>
       </ul>
