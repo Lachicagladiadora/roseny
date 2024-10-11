d02d981f2a00470709eba171d5361ba57a15d205
Lachicagladiadora <dev.roseny@gmail.com>
2024-10-10 23:52:08 -0500

    chore: add menu and functionality

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import { Sun } from "./icons/Sun";
 import { Moon } from "./icons/Moon";
 import { Close } from "./icons/Close";
 import { Hamburger } from "./icons/Hamburger";
+import { Menu } from "./Menu";
 
 export const Header = () => {
   const [isOpenMenu, setIsOpenMenu] = useState(false);
@@ -16,26 +17,37 @@ export const Header = () => {
   }, [isDarkTheme]);
 
   return (
-    <div className="w-full flex items-center justify-center py-6 border-b-2 border-b-black dark:border-b-white">
-      <header className="w-full max-w-[800px] flex items-center justify-between">
-        <button
-          className="h-8 w-8 text-3xl p-[1px] text-black dark:text-zinc-50"
-          onClick={() => setIsOpenMenu((p) => !p)}
-        >
-          {isOpenMenu ? <Close /> : <Hamburger />}
-        </button>
-        <h1 className="flex-1 text-center">Blogüi</h1>
-        <button
-          className={`h-8 w-8 text-3xl p-[1px] rounded-full text-black dark:text-zinc-50 ${
-            isDarkTheme
-              ? "hover:bg-blue-950 hover:text-sky-300"
-              : "hover:bg-yellow-50 hover:text-yellow-500"
-          }`}
-          onClick={() => setIsDarkTheme((p) => !p)}
-        >
-          {isDarkTheme ? <Moon /> : <Sun />}
-        </button>
-      </header>
-    </div>
+    <>
+      <div className="w-full flex items-center justify-center py-6 border-b-2 border-b-black dark:border-b-white">
+        <header className="w-full max-w-[900px] px-2 flex items-center justify-between relative">
+          <button
+            className="h-8 w-8 text-3xl p-[1px] text-black dark:text-zinc-50"
+            onClick={() => setIsOpenMenu((p) => !p)}
+          >
+            {isOpenMenu ? <Close /> : <Hamburger />}
+          </button>
+          <h1 className="flex-1 text-center">Blogüi</h1>
+          <button
+            className={`h-8 w-8 text-3xl p-[1px] rounded-full text-black dark:text-zinc-50 ${
+              isDarkTheme
+                ? "hover:bg-blue-950 hover:text-sky-300"
+                : "hover:bg-yellow-50 hover:text-yellow-500"
+            }`}
+            onClick={() => setIsDarkTheme((p) => !p)}
+          >
+            {isDarkTheme ? <Moon /> : <Sun />}
+          </button>
+        </header>
+      </div>
+      <div className="w-full h-0 flex items-center justify-center">
+        <div className="w-full max-w-[900px] flex items-center justify-center relative">
+          {isOpenMenu && (
+            <div className="w-[100vw] h-[100vh] absolute top-[0px] left-0  md:top-[0px] md:w-96 md:h-[600px] bg-black/10">
+              <Menu />
+            </div>
+          )}
+        </div>
+      </div>
+    </>
   );
 };
---
+++ src/components/Menu.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Menu = () => {
+  return <div className="h-full w-full bg-red-400 ">Menu</div>;
+};
