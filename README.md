76255004c0aa00dc994bd9c7ccfd9bfaa0a40e59
Lachicagladiadora <dev.roseny@gmail.com>
2024-11-11 21:19:10 -0500

    build: change to cloudflare

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Knowbud
+
+Website created with Astro, React and TypeScript with the purpose of sharing development concepts that were useful to me and I hope will be useful to someone.
+
+## Features
+
+## Collaborations
--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,15 @@ import tailwind from "@astrojs/tailwind";
 import react from '@astrojs/react';
 import icon from "astro-icon";
 
-import netlify from "@astrojs/netlify";
+// import netlify from "@astrojs/netlify";
+import cloudflare from '@astrojs/cloudflare';
+
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://lcg-knowbud.netlify.app",
   integrations: [tailwind(), react(), icon()],
   output: "server",
-  adapter: netlify()
+  // adapter: netlify()
+  adapter: cloudflare(),
 });
