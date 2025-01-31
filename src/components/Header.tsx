4204c9493735181ea3ef7f3235fbc1a9c4073de7
Lachicagladiadora <dev.roseny@gmail.com>
2025-01-30 23:06:30 -0500

    chore: remove unecessary comments and advance update theme handler

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,58 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { Sun } from "./icons/Sun";
 import { Moon } from "./icons/Moon";
+import { Menu } from "./Menu";
 
 export const Header = () => {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+    setIsDarkTheme(Boolean(theme));
+  }, []);
 
   return (
     <>
       <div className="w-full flex items-center justify-center py-2 bg-blank-light dark:bg-obscure-dark md:py-4">
         <header className="w-full max-w-[900px] px-4 flex items-center justify-between relative">
-          <div className="w-full">
-            <ul className="w-full flex items-center gap-6">
-              <li>
-                <a
-                  href="/"
-                  className="font-bold hover:text-obscure-light dark:hover:text-blank-dark"
-                >
-                  Home
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/portfolio"
-                  className="font-bold hover:text-obscure-light dark:hover:text-blank-dark"
-                >
-                  Portfolio
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/blog"
-                  className="font-bold hover:text-obscure-light dark:hover:text-blank-dark"
-                >
-                  Blog
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/contact"
-                  className="font-bold hover:text-obscure-light dark:hover:text-blank-dark"
-                >
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
+          <Menu />
           <button
             className="h-[36px] w-[36px] text-xl md:text-2xl lg:3xl p-[2px] rounded-full flex items-center justify-center transition text-obscure hover:bg-first-light hover:text-blank-light dark:text-second-dark dark:hover:bg-blank dark:hover:text-obscure-light"
             onClick={() => {
               const bodyElement = document.querySelector("html");
               if (!bodyElement) return;
-              setIsDarkTheme((p) => !p);
               bodyElement.classList.toggle("dark");
+              setIsDarkTheme((p) => {
+                localStorage.setItem("theme", `${!p}`);
+                return !p;
+              });
             }}
           >
             {isDarkTheme ? <Sun /> : <Moon />}
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,60 +2,36 @@ import React from "react";
 
 export const Menu = () => {
   return (
-    <div
-      className="w-full py-2 px-1 border-0  border-obscure-dark text-obscure-dark bg-second-light z-10
-      dark:border-blank-light dark:text-blank-light dark:bg-obscure-light 
-        md:rounded-2xl md:border-[2px]
-      "
-    >
-      <h3 className="py-6 text-lg font-bold text-center">Menu</h3>
-      <ul className="flex flex-col justify-center">
-        <li className="">
+    <div className="w-full">
+      <ul className="w-full flex items-center gap-6">
+        <li>
           <a
             href="/"
-            className="block py-4 px-2 text-center border-t-4 border-b-2 last:border-b-0 border-second 
-        hover:bg-first/80 
-        dark:border-first dark:hover:bg-blank dark:hover:text-obscure-dark dark:hover:border-second"
+            className="font-bold hover:text-obscure-light dark:hover:text-blank-dark"
           >
             Home
           </a>
         </li>
-        <li className="">
+        <li>
           <a
             href="/portfolio"
-            className="block py-4 px-2 text-center border-t-2 border-b-2 last:border-b-0 border-second 
-        hover:bg-first/80 
-        dark:border-first dark:hover:bg-blank dark:hover:text-obscure-dark dark:hover:border-second"
+            className="font-bold hover:text-obscure-light dark:hover:text-blank-dark"
           >
             Portfolio
           </a>
         </li>
-        {/* <li className="">
-          <a
-            href="/tags"
-            className="block py-4 px-2 text-center border-t-2 border-b-2 last:border-b-0 border-second 
-        hover:bg-first/80 hover:text-blank-light
-        dark:border-first dark:hover:bg-blank dark:hover:text-obscure-dark dark:hover:border-second"
-          >
-            Tags
-          </a>
-        </li> */}
-        <li className="">
+        <li>
           <a
             href="/blog"
-            className="block py-4 px-2 text-center border-t-2 border-b-2 last:border-b-0 border-second 
-        hover:bg-first/80
-        dark:border-first dark:hover:bg-blank dark:hover:text-obscure-dark dark:hover:border-second"
+            className="font-bold hover:text-obscure-light dark:hover:text-blank-dark"
           >
             Blog
           </a>
         </li>
-        <li className="">
+        <li>
           <a
             href="/contact"
-            className="block py-4 px-2 text-center border-t-2 border-b-2 last:border-b-0 last:rounded-b-xl border-second 
-        hover:bg-first/80
-        dark:border-first dark:hover:bg-blank dark:hover:text-obscure-dark dark:hover:border-second"
+            className="font-bold hover:text-obscure-light dark:hover:text-blank-dark"
           >
             Contact
           </a>
