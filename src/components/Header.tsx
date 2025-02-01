8059a4212e6d6ba03919dbf4d639b7761f64600a
Lachicagladiadora <dev.roseny@gmail.com>
2025-01-31 20:13:14 -0500

    chore: improve menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,11 @@ import { Sun } from "./icons/Sun";
 import { Moon } from "./icons/Moon";
 import { Menu } from "./Menu";
 
+export type Page = "Home" | "Portfolio" | "Blog" | "Contact";
+
 export const Header = () => {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [currentPage, setCurrentPage] = useState<Page>("Home");
 
   const updatedTheme = () => {
     const theme = localStorage.getItem("theme");
@@ -20,11 +23,20 @@ export const Header = () => {
     htmlElement?.classList.toggle("dark");
   }, []);
 
+  useEffect(() => {
+    // const htmlElement = document.querySelector("html");
+    const page = localStorage.getItem("page") ?? "Home";
+    setCurrentPage(page as Page);
+
+    // setIsDarkTheme(Boolean(theme));
+    // htmlElement?.classList.toggle("dark");
+  }, []);
+
   return (
     <>
       <div className="w-full flex items-center justify-center py-2 bg-blank-light dark:bg-obscure-dark md:py-4">
         <header className="w-full max-w-[900px] px-4 flex items-center justify-between relative">
-          <Menu />
+          <Menu currentPage={currentPage} fnChangePage={() => setCurrentPage} />
           <button
             className="h-[36px] w-[36px] text-xl md:text-2xl lg:3xl p-[2px] rounded-full flex items-center justify-center transition text-obscure hover:bg-first-light hover:text-blank-light dark:text-second-dark dark:hover:bg-blank dark:hover:text-obscure-light"
             onClick={() => {
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,40 +1,60 @@
 import React from "react";
+import type { Page } from "./Header";
+import { navigate } from "astro:transitions/client";
 
-export const Menu = () => {
+type MenuProps = { currentPage: Page; fnChangePage: (_p: Page) => void };
+
+export const Menu = ({ currentPage, fnChangePage }: MenuProps) => {
   return (
     <div className="w-full">
       <ul className="w-full flex items-center gap-6">
-        <li>
-          <a
-            href="/"
-            className="font-bold hover:text-obscure-light dark:hover:text-blank-dark"
-          >
-            Home
-          </a>
+        <li
+          className={` hover:text-obscure-light dark:hover:text-blank-dark
+              ${currentPage === "Home" ? "font-bold" : ""}
+              `}
+          onClick={() => {
+            fnChangePage("Home");
+            localStorage.setItem("page", "Home");
+            navigate("/");
+          }}
+        >
+          Home
         </li>
-        <li>
-          <a
-            href="/portfolio"
-            className="font-bold hover:text-obscure-light dark:hover:text-blank-dark"
-          >
-            Portfolio
-          </a>
+        <li
+          className={` hover:text-obscure-light dark:hover:text-blank-dark
+              ${currentPage === "Portfolio" ? "font-bold" : ""}
+              `}
+          onClick={() => {
+            fnChangePage("Portfolio");
+            localStorage.setItem("page", "Portfolio");
+            navigate("/portfolio");
+          }}
+        >
+          Portfolio
         </li>
-        <li>
-          <a
-            href="/blog"
-            className="font-bold hover:text-obscure-light dark:hover:text-blank-dark"
-          >
-            Blog
-          </a>
+        <li
+          className={` hover:text-obscure-light dark:hover:text-blank-dark
+              ${currentPage === "Blog" ? "font-bold" : ""}
+              `}
+          onClick={() => {
+            fnChangePage("Blog");
+            localStorage.setItem("page", "Blog");
+            navigate("/blog");
+          }}
+        >
+          Blog
         </li>
-        <li>
-          <a
-            href="/contact"
-            className="font-bold hover:text-obscure-light dark:hover:text-blank-dark"
-          >
-            Contact
-          </a>
+        <li
+          className={` hover:text-obscure-light dark:hover:text-blank-dark
+              ${currentPage === "Contact" ? "font-bold" : ""}
+              `}
+          onClick={() => {
+            fnChangePage("Contact");
+            localStorage.setItem("page", "Contact");
+            navigate("/contact");
+          }}
+        >
+          Contact
         </li>
       </ul>
     </div>
