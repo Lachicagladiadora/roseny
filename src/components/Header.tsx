f60edcc5e0c7a313ce40e964423a135a2b45fa3d
Lachicagladiadora <dev.roseny@gmail.com>
2025-02-12 23:01:40 -0500

    wip: home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,55 +1,10 @@
-import React, { useEffect, useState } from "react";
+import type { ReactNode } from "react";
 
-import { Sun } from "./icons/Sun";
-import { Moon } from "./icons/Moon";
-import { Menu } from "./Menu";
-
-export type Page = "Home" | "Portfolio" | "Blog" | "Contact";
-
-export const Header = () => {
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
-  const [currentPage, setCurrentPage] = useState<Page>("Home");
-
-  const updatedTheme = () => {
-    const theme = localStorage.getItem("theme");
-    setIsDarkTheme(Boolean(theme));
-  };
-
-  useEffect(() => {
-    const htmlElement = document.querySelector("html");
-    const theme = localStorage.getItem("theme");
-
-    setIsDarkTheme(Boolean(theme));
-    htmlElement?.classList.toggle("dark");
-  }, []);
-
-  useEffect(() => {
-    const page = localStorage.getItem("page") ?? "Home";
-    setCurrentPage(page as Page);
-  }, []);
-
+type HeaderProps = { children: ReactNode };
+export const Header = ({ children }: HeaderProps) => {
   return (
-    <>
-      <div className="w-full flex items-center justify-center py-2 bg-blank-light dark:bg-obscure-dark md:py-4">
-        <header className="w-full max-w-[900px] px-4 flex items-center justify-between relative">
-          <Menu currentPage={currentPage} fnChangePage={() => setCurrentPage} />
-          <button
-            className="h-[36px] w-[36px] text-xl md:text-2xl lg:3xl p-[2px] rounded-full flex items-center justify-center transition text-obscure hover:bg-first-light hover:text-blank-light dark:text-second-dark dark:hover:bg-blank dark:hover:text-obscure-light"
-            onClick={() => {
-              const bodyElement = document.querySelector("html");
-              if (!bodyElement) return;
-              bodyElement.classList.toggle("dark");
-              setIsDarkTheme((p) => {
-                localStorage.setItem("theme", `${!p}`);
-                return !p;
-              });
-              updatedTheme();
-            }}
-          >
-            {isDarkTheme ? <Sun /> : <Moon />}
-          </button>
-        </header>
-      </div>
-    </>
+    <header className="w-full max-w-[900px] px-4 flex items-center justify-between relative">
+      {children}
+    </header>
   );
 };
