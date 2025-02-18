0145a721031f05497e032cbeb276de7c989614a9
Lachicagladiadora <dev.roseny@gmail.com>
2025-02-17 23:39:02 -0500

    wip: home page and divide into sheets

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
 import type { ReactNode } from "react";
 
-type HeaderProps = { children: ReactNode };
-export const Header = ({ children }: HeaderProps) => {
-  return (
-    <header className="w-full max-w-[900px] px-4 flex items-center justify-between relative">
-      {children}
-    </header>
-  );
-};
+// type HeaderProps = { children: ReactNode };
+export const Header = () =>
+  // { children }: HeaderProps
+  {
+    return (
+      <header className="w-full max-w-[900px] px-4 flex items-center justify-between relative">
+        {/* {children} */}
+      </header>
+    );
+  };
