bf4313a2394ff84e8c63bc377044a0f7ff73a2b7
Lachicagladiadora <dev.roseny@gmail.com>
2025-03-03 20:16:20 -0500

    chore: advance design

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import type { ReactNode } from "react";
+// import type { ReactNode } from "react";
 
 // type HeaderProps = { children: ReactNode };
 export const Header = () =>
--- src/constants.ts
+++ src/constants.ts
@@ -1,82 +1,108 @@
-export const BLOG_NAME = "Knowbud";
+// export const BLOG_NAME = "Knowbud";
 
 export const MY_PROJECTS = [
   {
-    nameProject: "Diet Pixie",
-    about: "",
-    motivation: "",
-    description:
-      "App to see how your weight is and based on a goal such as 'maintaining your weight', it gives you a diet per day in grams.",
+    name: "Diet Pixie",
+    collaboration: false,
     coverImage: "/diet-pixie.png",
     urlProject: "https://diet-pixie.roseny.dev/",
     urlGitHub: "https://github.com/Lachicagladiadora/diet-pixie",
-    collaboration: false,
+    about: "",
+    description:
+      "App to see how your weight is and based on a goal such as 'maintaining your weight', it gives you a diet per day in grams.",
   },
   {
-    nameProject: "BitSip",
+    name: "Workout Tracker",
+    collaboration: true,
+    coverImage: "/workout-tracker.png",
+    urlProject: "https://pocket-budget.jimynicanor.com/",
+    urlGitHub: "",
     about: "",
-    motivation: "",
-    description: "App for recipes for food, desserts and drinks.",
+    description: "Web app for manage money",
+  },
+  {
+    name: "BitSip",
+    collaboration: false,
     coverImage: "/bitsip.png",
     urlProject: "https://bitsip.roseny.dev/",
     urlGitHub: "https://github.com/Lachicagladiadora/bitsip",
-    collaboration: false,
+    about: "",
+    description: "App for recipes for food, desserts and drinks.",
   },
   {
-    nameProject: "Pocket Budget",
-    about: "",
-    motivation: "",
-    description: "Web app for manage money",
+    name: "Pocket Budget",
+    collaboration: true,
+    //
     coverImage: "/pocket-budget.png",
     urlProject: "https://pocket-budget.jimynicanor.com/",
     urlGitHub: "",
+    about: "",
+    description: "Web app for manage money",
+  },
+  {
+    name: "Card Quest",
     collaboration: true,
+    coverImage: "/card-quest.png",
+    urlProject: "https://card-quest.jimynicanor.com/",
+    urlGitHub: "",
+    about: "",
+    description: "Web app for manage money",
   },
   {
-    nameProject: "Personal website",
-    about: "",
-    motivation: "",
-    description:
-      "Website created with Astro, React and TypeScript with the purpose of sharing development concepts that were useful to me and I hope will be useful to someone.",
-    coverImage: "/roseny-website.png",
-    urlProject: "https://roseny.dev/",
-    urlGitHub: "https://github.com/Lachicagladiadora/personal-website",
+    name: "ev component library",
+    collaboration: true,
+    coverImage: "/ev-component-library.png",
+    urlProject:
+      "https://ev-component-library.jimynicanor.com/?path=/docs/ev-component-library--docs",
+    urlGitHub: "",
+    about:
+      "Reusable component library designed to optimize web interface creation. Developed with React, TypeScript, and TailwindCSS, it includes detailed documentation in Storybook for easy implementation. Each component features style variants and customization options, ensuring design consistency. Available elements include simple components such as buttons with two variants and five color variations, adaptable to different needs. A solution focused on efficiency and reusability to accelerate web application development.",
+    description: "Web app for manage money",
+  },
+  {
+    name: "Cartago eCommerce",
+    collaboration: true,
+    coverImage: "/cartago-ecommerce.png",
+    urlProject: "https://cartago.vercel.app/",
+    urlGitHub: "https://github.com/DevCorvus/cartago",
+    about:
+      "Ecommerce.platform developed as a demonstration of collaboration in large-scale projects. Built from scratch with Next.js 14, NextAuth, TailwindCSS, Prisma (PostgreSQL), AWS S3, Zod, and Zustand, its architecture enables the management of products, orders, and differentiated roles. Three user types were defined: customers, who can add products to a wishlist, to the cart, and place orders; sellers, responsible for publishing and managing inventory; and administrators, overseeing category management and overall platform control. Although simplified compared to fully-fledged commercial solutions, this project emphasizes structure, scalability, and teamwork to build a more robust application",
+    description: "Web app for manage money",
+  },
+  {
+    name: "Hacker News",
     collaboration: false,
-  },
-  {
-    nameProject: "Hacker News",
-    about: "",
-    motivation: "",
-    description:
-      "Hacker News is a web application built using React and TypeScript, powered by the Hacker News API. It offers users two main sections: one for browsing stories across different programming languages, and another to view bookmarked stories. Each story includes a link to read the full article.",
     coverImage: "/hacker-news.png",
     urlProject: "https://hacker-news-delta-wine.vercel.app/",
     urlGitHub: "https://github.com/Lachicagladiadora/hacker-news",
-    collaboration: false,
+    about:
+      "Web application developed to facilitate access to technology news and allow saving interesting articles. Built with React, Vite, and TypeScript, it fetches real-time information from the Hacker News API. An infinite scroll system was implemented using Intersection Observer to enhance navigation, and localStorage was integrated to store favorite news. Additionally, unit and end-to-end (E2E) tests ensure the application's stability and correct functionality. A practical tool for discovering and organizing relevant technology content.",
+    description:
+      "Hacker News is a web application built using React and TypeScript, powered by the Hacker News API. It offers users two main sections: one for browsing stories across different programming languages, and another to view bookmarked stories. Each story includes a link to read the full article.",
   },
   {
-    nameProject: "Chuck Norris Jokes",
-    about: "",
-    motivation: "",
-    description:
-      "Practice DOM management, fetch and promises with only javascript and Chuck Norris API. view site. And deploy with GitHub-Pages",
+    name: "Chuck Norris Jokes",
+    collaboration: false,
     coverImage: "/chuck-norris-jokes.png",
     urlProject: "https://lachicagladiadora.github.io/chuck-norris-jokes/",
     urlGitHub: "https://github.com/Lachicagladiadora/chuck-norris-jokes",
-    collaboration: false,
+    about:
+      "Project developed to practice DOM manipulation, fetch, and promise handling in JavaScript. A request was made to the Chuck Norris API, and based on the response, elements in the interface were dynamically updated. Built exclusively with HTML, CSS, and JavaScript, the design was adapted to be responsive. In addition to showcasing technical skills, it provides a source of Chuck Norris jokes for fans.",
+    description:
+      "Practice DOM management, fetch and promises with only javascript and Chuck Norris API. view site. And deploy with GitHub-Pages",
   },
   {
-    nameProject: "Social Media Dashboard",
-    about: "",
-    motivation: "",
-    description:
-      "Web project for practice with HTML, CSS and some lines of JavaScript. Responsive layout and design practice that includes dark mode and light mode with JavaScript logic.",
+    name: "Social Media Dashboard",
+    collaboration: false,
     coverImage: "/social-media-dashboard.jpg",
     urlProject:
       "https://lachicagladiadora.github.io/dashboard-project-html-css/",
     urlGitHub:
       "https://github.com/Lachicagladiadora/dashboard-project-html-css",
-    collaboration: false,
+    about:
+      "Project developed to improve skills in interface creation. Focused on implementing responsive design, dark mode, and CSS Grid, areas that initially posed challenges. Using only HTML, CSS, and a few lines of JavaScript for dark mode logic, a functional and adaptable interface was created. The result demonstrates the ability to apply key techniques in web design and development, overcoming initial challenges.",
+    description:
+      "Web project for practice with HTML, CSS and some lines of JavaScript. Responsive layout and design practice that includes dark mode and light mode with JavaScript logic.",
   },
 
   // {
