57e8db587897e29be0a1a90b357596617bf42855
Lachicagladiadora <dev.roseny@gmail.com>
2025-03-10 18:02:55 -0500

    chore: change font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,19 +7,14 @@ export default {
 			'sans': ['ui-sans-serif', 'system-ui'],
 			'serif': ['ui-serif', 'Georgia'],
 			'mono': ['ui-monospace', 'SFMono-Regular'],
-			"overpass-mono": ["Overpass Mono"],
-			"montserrat": ["Montserrat Alternates"],
+			"raleway": ["Raleway", "sans-serif"],
 		},
 		colors: {
-
 			'obscure': '#252238',
-
 			'blank': '#D9D9D9',
 			'blank-dark': '#9CA2C3',
 			'blank-light': '#CFDCE7',
-
 			// 'first': '#252238',
-
 			'second': '#CF9065',
 			'second-dark': '#63815E',
 			'second-light': '#C37376'
