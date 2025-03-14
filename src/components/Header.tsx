e0ce52e351a46fb0566f299b8dec4606fbc880c6
Lachicagladiadora <dev.roseny@gmail.com>
2025-03-14 00:00:12 -0500

    fix: home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,7 @@
 export const Header = () => {
   return (
     <header className="w-full max-w-[1000px] mx-auto px-8 py-4 flex items-center justify-between relative text-blank-light">
-      <a href="mailto:dev.roseny@gmail.com" className="hover:text-second-dark">
-        email
-      </a>
+      <p className="hover:text-second-dark">dev.roseny@gmail.com</p>
     </header>
   );
 };
